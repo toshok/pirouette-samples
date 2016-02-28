b1143d9f455a6fac93955a2658cc2f31de55338b
Chris Toshok <toshok@gmail.com>
2016-02-28 15:50:11 -0800

    clean up this file a touch

--- ios/ios-test-es6/helloiostableview.js
+++ ios/ios-test-es6/helloiostableview.js
@@ -1,4 +1,6 @@
-console.log ("inside helloiostableview.js");
+/* -*- Mode: js2; tab-width: 4; indent-tabs-mode: nil; -*-
+ * vim: set ts=4 sw=4 et tw=99 ft=js:
+ */
 
 import { NSBundle, NSObject, NSRect } from '$pirouette/foundation';
 import { chainCtor, instanceSelector, sig } from '$pirouette/objc';
@@ -24,21 +26,21 @@ let HelloIOSViewController = ui.UIViewController.extendClass("HelloIOSViewContro
 let GLKCanvasViewController = GLKViewController.extendClass("GLKCanvasViewController", () => ({
 
     constructor: function (handle) {
-      chainCtor(GLKCanvasViewController, this, arguments);
-      if (!handle)
-        this.initWithNibNameAndBundle(null, null);
+        chainCtor(GLKCanvasViewController, this, arguments);
+        if (!handle)
+            this.initWithNibNameAndBundle(null, null);
     },
 
     loadView: instanceSelector().returns(() => sig.Void).impl(function() {
-                this.view = new GLKCanvasView().initWithFrame(ui.UIScreen.mainScreen.bounds);
-                this.view.drawableDepthFormat = GLKViewDrawableDepthFormat.depth16;
-              })
+        this.view = new GLKCanvasView().initWithFrame(ui.UIScreen.mainScreen.bounds);
+        this.view.drawableDepthFormat = GLKViewDrawableDepthFormat.depth16;
+    })
 }));
 
 let TargetActionProxy1 = NSObject.extendClass("TargetActionProxy1", () => ({
     constructor: function(fn) {
-      chainCtor(TargetActionProxy1, this);
-      this.fn = fn;
+        chainCtor(TargetActionProxy1, this);
+        this.fn = fn;
     },
 
     proxyAction: instanceSelector("action").returns(() => sig.Void).impl (function (a1) { return this.fn(a1); })
@@ -47,174 +49,171 @@ let TargetActionProxy1 = NSObject.extendClass("TargetActionProxy1", () => ({
 let HelloIOSAppDelegate = NSObject.extendClass("HelloIOSAppDelegate", () => ({
 
     constructor: function () {
-      console.log ("made it to HelloIOSAppDelegate.ctor");
-      chainCtor(HelloIOSAppDelegate, this, arguments);
+        console.log ("made it to HelloIOSAppDelegate.ctor");
+        chainCtor(HelloIOSAppDelegate, this, arguments);
     },
 
     runGLDemo: function(demoName, demo) {
-	function uiAlertOnException(f) {
+        function uiAlertOnException(f) {
             try {
-		f();
-		return true;
+                f();
+                return true;
             } catch (e) {
-		console.log(e);
-		new ui.UIAlertView().init("Exception in demo script", e, null, "Ok", null).show();
-		return false;
+                console.log(e);
+                new ui.UIAlertView().init("Exception in demo script", e, null, "Ok", null).show();
+                return false;
             }
-	};
+        };
 
-	let glkcontroller = new GLKCanvasViewController;
-	let canvas = glkcontroller.view;
+        let glkcontroller = new GLKCanvasViewController;
+        let canvas = glkcontroller.view;
 
-	glkcontroller.delegate = {
-	    update: typeof(demo.update) === "function" ? demo.update : () => void 0
-	};
+        glkcontroller.delegate = {
+            update: typeof(demo.update) === "function" ? demo.update : () => void 0
+        };
 
-	canvas.delegate = {
-	    drawInRect: demo.draw
-	};
+        canvas.delegate = {
+            drawInRect: demo.draw
+        };
 
-	if (demo.tap) {
-	    let tapProxy = new TargetActionProxy1(demo.tap);
+        if (demo.tap) {
+            let tapProxy = new TargetActionProxy1(demo.tap);
             canvas.addGestureRecognizer(new ui.UITapGestureRecognizer().initWithTarget(tapProxy, tapProxy.proxyAction));
-	}
+        }
 
-	demo.run(canvas);
+        demo.run(canvas);
 
-	glkcontroller.title = "" + demoName;
-	this.window.rootViewController.pushViewController(glkcontroller, true);
+        glkcontroller.title = "" + demoName;
+        this.window.rootViewController.pushViewController(glkcontroller, true);
     },
 
     workerDemo: function() {
-      let screenBounds = ui.UIScreen.mainScreen.bounds;
-      let newcontroller = new HelloIOSViewController("HelloIOSViewController", null);
-      let primeButton = ui.UIButton.buttonWithType(ui.UIButtonType.roundedRect);
-      primeButton.title = "Click to generate primes";
-      primeButton.frame = new NSRect(screenBounds.width / 2 - 100, screenBounds.height / 2 - 50, 200, 50);
+        let screenBounds = ui.UIScreen.mainScreen.bounds;
+        let newcontroller = new HelloIOSViewController("HelloIOSViewController", null);
+        let primeButton = ui.UIButton.buttonWithType(ui.UIButtonType.roundedRect);
+        primeButton.title = "Click to generate primes";
+        primeButton.frame = new NSRect(screenBounds.width / 2 - 100, screenBounds.height / 2 - 50, 200, 50);
 
-      let primeTextField = new ui.UITextField().initWithFrame(new NSRect(0, screenBounds.height / 2 + 50, screenBounds.width, 50));
-      primeTextField.textAlignment = ui.UITextAlignment.center;
+        let primeTextField = new ui.UITextField().initWithFrame(new NSRect(0, screenBounds.height / 2 + 50, screenBounds.width, 50));
+        primeTextField.textAlignment = ui.UITextAlignment.center;
 
-      let primeCount = 0;
+        let primeCount = 0;
 
-      let worker;
+        let worker;
 
-      primeButton.clicked = () => {
-        if (worker) {
-          primeButton = "Click to generate primes";
-          primeTextField.text = "";
-          worker.close();
-          worker = null;
-        } else {
-          primeButton = "Click to stop";
-/*
-          worker = new Worker('./prime-worker');
-          worker.onmessage = (msg) => {
-            primeTextField.text = "Prime " + (++primeCount) + ": " + msg;
-          };
-*/
-        }
-      };
+        primeButton.clicked = () => {
+            if (worker) {
+                primeButton = "Click to generate primes";
+                primeTextField.text = "";
+                worker.close();
+                worker = null;
+            } else {
+                primeButton = "Click to stop";
+                /*
+                 worker = new Worker('./prime-worker');
+                 worker.onmessage = (msg) => {
+                 primeTextField.text = "Prime " + (++primeCount) + ": " + msg;
+                 };
+                 */
+            }
+        };
 
-      newcontroller.title = "Web Workers";
-      newcontroller.view.addSubviews(primeButton, primeTextField);
-      this.window.rootViewController.pushViewController(newcontroller, true);
+        newcontroller.title = "Web Workers";
+        newcontroller.view.addSubviews(primeButton, primeTextField);
+        this.window.rootViewController.pushViewController(newcontroller, true);
     },
 
     xhrDemo: function() {
-      let screenBounds = ui.UIScreen.mainScreen.bounds;
-      let newcontroller = new ui.UIViewController();
-      newcontroller.view.backgroundColor = ui.UIColor.whiteColor;
-      newcontroller.title = "XMLHttpRequest";
+        let screenBounds = ui.UIScreen.mainScreen.bounds;
+        let newcontroller = new ui.UIViewController();
+        newcontroller.view.backgroundColor = ui.UIColor.whiteColor;
+        newcontroller.title = "XMLHttpRequest";
 
-      let xhrButton = ui.UIButton.buttonWithType(ui.UIButtonType.roundedRect);
-      xhrButton.title = "Click to fetch google.com";
-      xhrButton.frame = new NSRect(screenBounds.width / 2 - 100, screenBounds.height / 2 - 50, 250, 50);
+        let xhrButton = ui.UIButton.buttonWithType(ui.UIButtonType.roundedRect);
+        xhrButton.title = "Click to fetch google.com";
+        xhrButton.frame = new NSRect(screenBounds.width / 2 - 100, screenBounds.height / 2 - 50, 250, 50);
 
-      let xhrTextView = new ui.UITextView().initWithFrame(new NSRect(0, screenBounds.height / 2 + 50, screenBounds.width, screenBounds.height - (screenBounds.height / 2 + 50)), null);
-      xhrTextView.textAlignment = ui.UITextAlignment.center;
+        let xhrTextView = new ui.UITextView().initWithFrame(new NSRect(0, screenBounds.height / 2 + 50, screenBounds.width, screenBounds.height - (screenBounds.height / 2 + 50)), null);
+        xhrTextView.textAlignment = ui.UITextAlignment.center;
 
-      xhrButton.clicked = () => {
-	console.log ("clicked!");
-        let xhr = new XMLHttpRequest;
-        xhr.open("GET", "https://google.com/");
-        xhr.onreadystatechange = () => {
-	  console.log ("onreadystatechange!");
-          if (xhr.readyState === 4) {
-	      console.log ("response = " + xhr.responseText);
-	      console.log ("status = " + xhr.statusText);
-              xhrTextView.text = "response = " + xhr.responseText;
-	  }
+        xhrButton.clicked = () => {
+            console.log ("clicked!");
+            let xhr = new XMLHttpRequest;
+            xhr.open("GET", "https://google.com/");
+            xhr.onreadystatechange = () => {
+                console.log ("onreadystatechange!");
+                if (xhr.readyState === 4) {
+                    console.log ("response = " + xhr.responseText);
+                    console.log ("status = " + xhr.statusText);
+                    xhrTextView.text = "response = " + xhr.responseText;
+                }
+            };
+            console.log ("sending!");
+            xhr.send();
         };
-	  console.log ("sending!");
-        xhr.send();
-      };
-      newcontroller.view.addSubview(xhrButton);
-      newcontroller.view.addSubview(xhrTextView);
-      this.window.rootViewController.pushViewController(newcontroller, true);
+        newcontroller.view.addSubview(xhrButton);
+        newcontroller.view.addSubview(xhrTextView);
+        this.window.rootViewController.pushViewController(newcontroller, true);
     },
 
     didFinishLaunching: instanceSelector("application:didFinishLaunchingWithOptions:").impl(function() {
-      console.log ("inside HelloIOAppDelegate.didFinishLaunching");
-      this.window = new ui.UIWindow().initWithFrame(ui.UIScreen.mainScreen.bounds);
+        console.log ("inside HelloIOAppDelegate.didFinishLaunching");
+        this.window = new ui.UIWindow().initWithFrame(ui.UIScreen.mainScreen.bounds);
 
-      let tableviewcontroller = new ui.UITableViewController().initWithStyle(ui.UITableViewStyle.plain);
-      tableviewcontroller.title = "Pirouette Demos";
+        let tableviewcontroller = new ui.UITableViewController().initWithStyle(ui.UITableViewStyle.plain);
+        tableviewcontroller.title = "Pirouette Demos";
 
-      let tableData = [ /*{ title: "Web Workers",
+        let tableData = [ /*{ title: "Web Workers",
                            rows: [ { title: "Primes in a button",  clicked: () => this.workerDemo() } ] },*/
-                        { title: "XMLHttpRequest",
-                           rows: [ { title: "Simple fetch",        clicked: () => this.xhrDemo() } ] },
-                        { title: "WebGL",
-                           rows: [
-			       // { title: "MDN sample 2", 	   clicked: () => this.runGLDemo("sample2", mdn_sample2) },
-                               // { title: "MDN sample 3", 	   clicked: () => this.runGLDemo("sample3", mdn_sample3) },
-                               // { title: "MDN sample 4", 	   clicked: () => this.runGLDemo("sample4", mdn_sample4) },
-                               // { title: "MDN sample 5", 	   clicked: () => this.runGLDemo("sample5", mdn_sample5) },
-                               { title: "J3D Engine: Cube",    clicked: () => this.runGLDemo("Cube",    j3d_cube) },
-                               { title: "J3D Engine: Lights",  clicked: () => this.runGLDemo("Lights",  j3d_lights) },
-                               { title: "J3D Engine: Scene",   clicked: () => this.runGLDemo("Scene",   j3d_scene) },
-                               { title: "J3D Engine: Head",    clicked: () => this.runGLDemo("Head",    j3d_head) },
-                               { title: "J3D Engine: Cubemap", clicked: () => this.runGLDemo("Cubemap", j3d_cubemap) },
-                               { title: "J3D Engine: Plasma",  clicked: () => this.runGLDemo("Plasma",  j3d_plasma) }
-			   ] }
-                      ];
+            { title: "XMLHttpRequest",
+              rows: [ { title: "Simple fetch",        clicked: () => this.xhrDemo() } ] },
+            { title: "WebGL",
+              rows: [
+                  // { title: "MDN sample 2",     clicked: () => this.runGLDemo("sample2", mdn_sample2) },
+                  // { title: "MDN sample 3",     clicked: () => this.runGLDemo("sample3", mdn_sample3) },
+                  // { title: "MDN sample 4",     clicked: () => this.runGLDemo("sample4", mdn_sample4) },
+                  // { title: "MDN sample 5",     clicked: () => this.runGLDemo("sample5", mdn_sample5) },
+                  { title: "J3D Engine: Cube",    clicked: () => this.runGLDemo("Cube",    j3d_cube) },
+                  { title: "J3D Engine: Lights",  clicked: () => this.runGLDemo("Lights",  j3d_lights) },
+                  { title: "J3D Engine: Scene",   clicked: () => this.runGLDemo("Scene",   j3d_scene) },
+                  { title: "J3D Engine: Head",    clicked: () => this.runGLDemo("Head",    j3d_head) },
+                  { title: "J3D Engine: Cubemap", clicked: () => this.runGLDemo("Cubemap", j3d_cubemap) },
+                  { title: "J3D Engine: Plasma",  clicked: () => this.runGLDemo("Plasma",  j3d_plasma) }
+              ] }
+        ];
 
-      let pathToItem = (arr, pos, path) => {
-        let item = arr[path.indexAtPosition(pos)];
-        if (pos === (path.length - 1)) {
-          return item;
-        } else {
-          return pathToItem(item.rows, pos + 1, path);
-        }
-      };
-      tableviewcontroller.tableView.delegate = {
-        didSelectRow:               (tv, path) => pathToItem(tableData, 0, path).clicked()
-      };
-      tableviewcontroller.tableView.dataSource = {
-        numberOfSections:                   () => tableData.length,
-        numberOfRowsInSection:   (tv, section) => tableData[section].rows.length,
-        titleForHeaderInSection: (tv, section) => tableData[section].title,
-        cellForRow:                 (tv, path) => {
-          let cell = tv.dequeueReusableCellWithIdentifier("reuse") || new ui.UITableViewCell().initWithStyle(ui.UITableViewCellStyle.value1, "reuse");
-          cell.textLabel.text = pathToItem(tableData, 0, path).title;
-          return cell;
-        }
-      };
+        let pathToItem = (arr, pos, path) => {
+            let item = arr[path.indexAtPosition(pos)];
+            if (pos === (path.length - 1)) {
+                return item;
+            } else {
+                return pathToItem(item.rows, pos + 1, path);
+            }
+        };
+        tableviewcontroller.tableView.delegate = {
+            didSelectRow:               (tv, path) => pathToItem(tableData, 0, path).clicked()
+        };
+        tableviewcontroller.tableView.dataSource = {
+            numberOfSections:                   () => tableData.length,
+            numberOfRowsInSection:   (tv, section) => tableData[section].rows.length,
+            titleForHeaderInSection: (tv, section) => tableData[section].title,
+            cellForRow:                 (tv, path) => {
+                let cell = tv.dequeueReusableCellWithIdentifier("reuse") || new ui.UITableViewCell().initWithStyle(ui.UITableViewCellStyle.value1, "reuse");
+                cell.textLabel.text = pathToItem(tableData, 0, path).title;
+                return cell;
+            }
+        };
 
-      let navController = new ui.UINavigationController().initWithRootViewController(tableviewcontroller);
-      navController.toolbarHidden = ui.UIDevice.currentDevice.userInterfaceIdiom !== ui.UIUserInterfaceIdiom.pad;
-      this.window.rootViewController = navController;
+        let navController = new ui.UINavigationController().initWithRootViewController(tableviewcontroller);
+        navController.toolbarHidden = ui.UIDevice.currentDevice.userInterfaceIdiom !== ui.UIUserInterfaceIdiom.pad;
+        this.window.rootViewController = navController;
 
-      this.window.makeKeyAndVisible();
-      return true;
+        this.window.makeKeyAndVisible();
+        return true;
     })
 }));
 
-console.log ("before chdir");
+// chdir to the mainbundle's path so the relative paths to all our j3d resources will work
 process.chdir(NSBundle.mainBundle.bundlePath);
-console.log ("after chdir");
 
-console.log ("before UIApplication.main, delegate name = " + HelloIOSAppDelegate._ck_register);
-ui.UIApplication.main(process.argv, null, "HelloIOSAppDelegate" /*HelloIOSAppDelegate._ck_register*/);
-console.log ("UIApplication.main returned, wtf");
+ui.UIApplication.main(process.argv, null, "HelloIOSAppDelegate");
